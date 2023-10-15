be5f4ea646b885badabe3ad8835c13e691bf1d62
yellowsman <sigesigeito@gmail.com>
2023-10-16 04:25:33 +0900

    変更の必要の無い箇所を戻す

--- lib/nimble_school_web/controllers/blog_controller.ex
+++ lib/nimble_school_web/controllers/blog_controller.ex
@@ -4,10 +4,10 @@ defmodule NimbleSchoolWeb.BlogController do
   alias NimbleSchool.Blog
 
   def index(conn, _params) do
-    render(conn, :index, posts: Blog.all_posts())
+    render(conn, "index.html", posts: Blog.all_posts())
   end
 
   def show(conn, %{"id" => id}) do
-    render(conn, :show, post: Blog.get_post_by_id!(id))
+    render(conn, "show.html", post: Blog.get_post_by_id!(id))
   end
 end
